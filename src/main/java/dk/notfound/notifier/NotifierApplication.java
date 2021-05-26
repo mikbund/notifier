153d59dd8b0d2a62e23827d03f17e930edef4b81
Mikkel Bundgaard <mikkel@notfound.dk>
2021-05-26 22:38:15 +0200

    version 0.1

--- src/main/java/dk/notfound/notifier/NotifierApplication.java
+++ src/main/java/dk/notfound/notifier/NotifierApplication.java
@@ -4,11 +4,12 @@ package dk.notfound.notifier;
 import dk.notfound.notifier.controller.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NotifierApplication {
+public class NotifierApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(NotifierApplication.class, args);
 	}
 
--- src/main/java/dk/notfound/notifier/model/EventRepository.java
+++ src/main/java/dk/notfound/notifier/model/EventRepository.java
@@ -1,12 +1,13 @@
 package dk.notfound.notifier.model;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-
-public interface EventRepository extends CrudRepository<Event, Long> {
+//CrudRepository
+public interface EventRepository extends JpaRepository<Event, Long> {
 
 
     Iterable<Event>  findAllByAcknowledged(Boolean acknowledged);
